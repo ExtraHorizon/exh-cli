b41f68f656ad068fc79bfdcc40a9411a9c9c6439
Kobe Leysen <kobe.leysen@fibricheck.com>
2021-12-13 17:14:31 +0100

    bump to 1.0.3

--- bin/index.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-
-let x = require('../build/index.js');
-
-x.exhCli();
--- src/index.ts
+++ src/index.ts
@@ -30,4 +30,6 @@ function checkForNewVersion() {
     });
   
     notifier.notify();
-  }
+}
+
+exhCli();
