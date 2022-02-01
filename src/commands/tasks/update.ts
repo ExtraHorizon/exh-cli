692f3a2fabde0d0f8da139eef919ac8c85237c77
Filip Aben <filip.aben@extrahorizon.com>
2022-02-01 09:04:18 +0100

    Workaround for task update
    Due to a problem in the backend, the update request needs to be split
    into 2 parts.

--- src/commands/tasks/update.ts
+++ src/commands/tasks/update.ts
@@ -1,3 +1,4 @@
+import chalk = require('chalk');
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -13,15 +14,23 @@ export const handler = async ({ sdk, name, code, entryPoint, runtime, descriptio
 
   const file = fs.readFileSync(path.join(process.cwd(), code));
 
-  const reponse = await sdk.raw.put(`/tasks/v1/functions/${name}`, {
+  /* Due to an issue in the backend, we can't update all data at once. Needs to be done in separate commands. Fix this once resolved */
+  const response1 = await sdk.raw.put(`/tasks/v1/functions/${name}`, {
     name,
     description: description || 'none',
-    code: file.toString('base64'),
     entryPoint,
     runtime,
     timeLimit,
     memoryLimit,
     environmentVariables: envArr,
   });
-  console.log(reponse.data);
+  const response2 = await sdk.raw.put(`/tasks/v1/functions/${name}`, {
+    code: file.toString('base64'),
+  });
+
+  if (response1.data?.affectedRecords && response2.data?.affectedRecords) {
+    console.log(chalk.green(`Updated task ${name}`));
+  } else {
+    console.log(chalk.red(`Failed to update task ${name}`));
+  }
 };
