b22ff829ae8da553296025bee29908bef09196f0
Filip Aben <filip.aben@extrahorizon.com>
2022-02-15 14:41:52 +0100

    fixes #9
    - Clarified that the --code parameter should be a zip file
    - Consistently refer to tasks instead of functions.

--- src/commands/tasks/create.ts
+++ src/commands/tasks/create.ts
@@ -8,12 +8,12 @@ export const desc = 'Create a new task';
 export const builder = (yargs: any) => epilogue(yargs).options({
   name: {
     demandOption: true,
-    describe: 'Name of the function',
+    describe: 'Name of the task',
     type: 'string',
   },
   code: {
     demandOption: true,
-    describe: 'File containing the code of the function',
+    describe: 'Zip\'d contents of the directory containing the (compile) task code',
     type: 'string',
   },
   entryPoint: {
@@ -28,22 +28,22 @@ export const builder = (yargs: any) => epilogue(yargs).options({
     type: 'string',
   },
   description: {
-    describe: 'A description for this function',
+    describe: 'A description for this task',
     type: 'string',
     default: '',
   },
   timeLimit: {
-    describe: 'The execution time limit for this function (in seconds). Min: 3 max: 900',
+    describe: 'The execution time limit for this task (in seconds). Min: 3 max: 900',
     type: 'number',
     default: 60,
   },
   memoryLimit: {
-    describe: 'The allocated memory for this function (in MB). Min: 128 max: 10240',
+    describe: 'The allocated memory for this task (in MB). Min: 128 max: 10240',
     type: 'number',
     default: 128,
   },
   env: {
-    describe: 'Environment Variables set for this function. This option can be used multiple times.',
+    describe: 'Environment Variables set for this task. This option can be used multiple times.',
     type: 'string',
     default: [],
   },
@@ -55,7 +55,7 @@ export const builder = (yargs: any) => epilogue(yargs).options({
     if (memoryLimit !== undefined && (memoryLimit < 128 || timeLimit > 10240)) {
       throw new Error('ERROR: memoryLimit out of bounds!');
     }
-    if (!/^[A-Za-z0-9]+/g.test(name)) throw new Error('Please use only alphanumberic characters for your function name');
+    if (!/^[A-Za-z0-9]+/g.test(name)) throw new Error('Please use only alphanumberic characters for your task name');
 
     if (!fs.existsSync(path.join(process.cwd(), code)) || !fs.statSync(path.join(process.cwd(), code)).isFile()) {
       throw new Error('please provide a valid file path for your code');
@@ -71,7 +71,7 @@ export const handler = async ({ sdk, name, code, entryPoint, runtime, descriptio
 
   const functions = (await sdk.raw.get('/tasks/v1/functions')).data.data;
   const myFunction = functions.find((f:any) => f.name === name);
-  if (myFunction) throw new Error('Function name already exists. Either specify a new name or use the update command to update the existing function');
+  if (myFunction) throw new Error('Task name already exists. Either specify a new name or use the update command to update the existing task');
 
   const response = await sdk.raw.post('/tasks/v1/functions', {
     name,
