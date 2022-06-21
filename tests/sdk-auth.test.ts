bef12ff23308bc69eaa16989ce5abd855cb20185
Filip Aben <filip.aben@extrahorizon.com>
2022-06-21 08:33:45 +0200

    chore: eslint fix

--- tests/sdk-auth.test.ts
+++ tests/sdk-auth.test.ts
@@ -6,16 +6,16 @@ jest.mock('fs');
 
 const readFileSyncMock = <jest.Mock>readFileSync;
 
-const env = process.env
+const { env } = process;
 
 beforeEach(() => {
-  jest.resetModules()
-  process.env = { ...env }
-})
+  jest.resetModules();
+  process.env = { ...env };
+});
 
 afterEach(() => {
-  process.env = env
-})
+  process.env = env;
+});
 
 test('No existing file and no env credentials should throw an error', async () => {
   readFileSyncMock.mockImplementationOnce(() => { throw new Error(''); });
@@ -28,42 +28,40 @@ test('Existing file with missing credentials should throw an error', async () =>
 });
 
 test('File with correct credentials should not throw an error', async () => {
-  readFileSyncMock.mockImplementationOnce(() => "API_HOST=test\r\nAPI_OAUTH_CONSUMER_KEY=test\r\nAPI_OAUTH_CONSUMER_SECRET=test\r\nAPI_OAUTH_TOKEN=testtoken\r\nAPI_OAUTH_TOKEN_SECRET=testsecret\r\n");
+  readFileSyncMock.mockImplementationOnce(() => 'API_HOST=test\r\nAPI_OAUTH_CONSUMER_KEY=test\r\nAPI_OAUTH_CONSUMER_SECRET=test\r\nAPI_OAUTH_TOKEN=testtoken\r\nAPI_OAUTH_TOKEN_SECRET=testsecret\r\n');
   await expect(sdkAuth()).resolves.toBe(sdkMock);
   const authCall = sdkMock.auth.authenticate;
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('testtoken')
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('testsecret')
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('testtoken');
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('testsecret');
 });
 
 test('Check that whitespaces are trimmed from credentials file', async () => {
-  readFileSyncMock.mockImplementationOnce(() => "   API_HOST   =   test   \r\n\
-API_OAUTH_CONSUMER_KEY = test \r\n       API_OAUTH_CONSUMER_SECRET=test\r\n    API_OAUTH_TOKEN   =  testtoken\r\n    API_OAUTH_TOKEN_SECRET  =   testsecret  \r\n   ");
+  readFileSyncMock.mockImplementationOnce(() => `   API_HOST   =   test   \r\n
+API_OAUTH_CONSUMER_KEY = test \r\n       API_OAUTH_CONSUMER_SECRET=test\r\n    API_OAUTH_TOKEN   =  testtoken\r\n    API_OAUTH_TOKEN_SECRET  =   testsecret  \r\n   `);
   await expect(sdkAuth()).resolves.toBe(sdkMock);
   const authCall = sdkMock.auth.authenticate;
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('testtoken')
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('testsecret')
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('testtoken');
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('testsecret');
 });
 
 test('Credentials file variables can be overridden by environment variables', async () => {
-  readFileSyncMock.mockImplementationOnce(() => "API_HOST=test\r\nAPI_OAUTH_CONSUMER_KEY=test\r\nAPI_OAUTH_CONSUMER_SECRET=test\r\nAPI_OAUTH_TOKEN=testtoken\r\nAPI_OAUTH_TOKEN_SECRET=testsecret\r\n");
-  process.env.API_OAUTH_TOKEN_SECRET = "overriddentokensecret";
+  readFileSyncMock.mockImplementationOnce(() => 'API_HOST=test\r\nAPI_OAUTH_CONSUMER_KEY=test\r\nAPI_OAUTH_CONSUMER_SECRET=test\r\nAPI_OAUTH_TOKEN=testtoken\r\nAPI_OAUTH_TOKEN_SECRET=testsecret\r\n');
+  process.env.API_OAUTH_TOKEN_SECRET = 'overriddentokensecret';
   await expect(sdkAuth()).resolves.toBe(sdkMock);
   delete process.env.API_OAUTH_TOKEN_SECRET;
   const authCall = sdkMock.auth.authenticate;
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('testtoken')
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('overriddentokensecret')
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('testtoken');
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('overriddentokensecret');
 });
 
 test('Supplement credentials file with environment variables to get valid auth', async () => {
-  readFileSyncMock.mockImplementationOnce(() => "API_HOST=test\r\nAPI_OAUTH_CONSUMER_KEY=test\r\nAPI_OAUTH_CONSUMER_SECRET=test\r\n");
-  process.env.API_OAUTH_TOKEN = "envtoken";
-  process.env.API_OAUTH_TOKEN_SECRET = "envtokensecret";
+  readFileSyncMock.mockImplementationOnce(() => 'API_HOST=test\r\nAPI_OAUTH_CONSUMER_KEY=test\r\nAPI_OAUTH_CONSUMER_SECRET=test\r\n');
+  process.env.API_OAUTH_TOKEN = 'envtoken';
+  process.env.API_OAUTH_TOKEN_SECRET = 'envtokensecret';
   await expect(sdkAuth()).resolves.toBe(sdkMock);
   delete process.env.API_OAUTH_TOKEN;
   delete process.env.API_OAUTH_TOKEN_SECRET;
   const authCall = sdkMock.auth.authenticate;
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('envtoken')
-  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('envtokensecret')
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].token).toBe('envtoken');
+  expect(authCall.mock.calls[authCall.mock.calls.length - 1][0].tokenSecret).toBe('envtokensecret');
 });
-
-
