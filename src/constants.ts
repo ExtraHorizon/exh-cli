0ce0b4d6e04e49d45e4f70807e270b5dce0e2aec
Wilco Waaijer <wilco.waaijer@extrahorizon.com>
2024-02-27 18:02:09 +0100

    Updated the supported runtimes for task functions

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,28 @@ import * as path from 'path';
 
 export const EXH_CONFIG_FILE_DIR = path.join(homedir(), '/.exh');
 export const EXH_CONFIG_FILE = `${EXH_CONFIG_FILE_DIR}/credentials`;
-export const runtimeChoices = ['nodejs12.x', 'nodejs14.x', 'nodejs16.x', 'nodejs18.x', 'python3.7', 'python3.8', 'python3.9', 'python3.10', 'java8', 'java8.al2', 'java11', 'java17', 'dotnetcore3.1', 'dotnet6', 'dotnet7', 'go1.x', 'ruby2.7', 'ruby3.2'];
+
+// From: https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
+export const runtimeChoices = [
+  'nodejs16.x',
+  'nodejs18.x',
+  'nodejs20.x',
+  'python3.8',
+  'python3.9',
+  'python3.10',
+  'python3.11',
+  'python3.12',
+  'java8.al2',
+  'java11',
+  'java17',
+  'java21',
+  'dotnet6',
+  'dotnet7',
+  'dotnet8',
+  'ruby3.2',
+  'provided.al2',
+  'provided.al2023',
+];
 
 export const limits = {
   time: {
