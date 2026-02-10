8ba1e3916f3ce9ac6582add8922a23ea7cb05346
Wilco <wilco.waaijer@extrahorizon.com>
2026-02-10 13:48:28 +0100

    Test and refactor templates list

--- src/commands/templates/list.ts
+++ src/commands/templates/list.ts
@@ -1,22 +1,27 @@
 import chalk = require('chalk');
-import { getSdk } from '../../exh';
 import { epilogue } from '../../helpers/util';
+import * as templateRepository from '../../repositories/templates';
 
 export const command = 'list';
 export const desc = 'List all templates';
 export const builder = (yargs: any) => epilogue(yargs);
 
 export const handler = async function list({ isTTY }: { isTTY: boolean; }) {
-  const templates = await getSdk().templates.findAll();
-  if (templates) {
-    if (!templates.length) {
-      console.log(chalk.red('No templates found'));
-      return;
-    }
-    if (isTTY) {
-      console.table(templates.map((c:any) => ({ Id: c.id, Name: c.name, Description: c.description || '<none>', 'Last updated': c.updateTimestamp.toISOString() })));
-    } else {
-      templates.forEach((f:any) => (console.log(f)));
-    }
+  const templates = await templateRepository.findAll();
+
+  if (templates.length < 1) {
+    console.log(chalk.red('No templates found'));
+    return;
+  }
+
+  if (isTTY) {
+    console.table(templates.map(template => ({
+      Id: template.id,
+      Name: template.name,
+      Description: template.description || '<none>',
+      'Last updated': template.updateTimestamp.toISOString(),
+    })));
+  } else {
+    templates.forEach(template => console.log(template));
   }
 };
