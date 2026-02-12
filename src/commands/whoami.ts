c63f6e63873d701de1a3342010497795f0663e56
Wilco <wilco.waaijer@extrahorizon.com>
2026-02-12 12:36:30 +0100

    Split yargs from the root commands logic

--- src/commands/whoami.ts
+++ src/commands/whoami.ts
@@ -1,19 +1,10 @@
 import { epilogue } from '../helpers/util';
-import * as authRepository from '../repositories/auth';
+import * as authService from '../services/auth';
 
 export const command = 'whoami';
 export const desc = 'Shows the currently logged in user';
 export const builder = (yargs: any) => epilogue(yargs);
 
-export const handler = async function list() {
-  const host = authRepository.getHost();
-  if (!host) {
-    console.log('No ExH cluster host was found in the configuration.');
-    return;
-  }
-
-  console.log('You are targeting:', host);
-
-  const currentUser = await authRepository.fetchMe();
-  console.log('You are logged in as:', currentUser.email);
+export const handler = async function whoami() {
+  await authService.whoami();
 };
