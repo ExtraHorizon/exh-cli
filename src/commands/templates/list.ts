0c0b5e4c828f598634869aa26f2488be0554c9e9
Wilco <wilco.waaijer@extrahorizon.com>
2026-02-12 10:54:23 +0100

    Split the templates logic from yargs files

--- src/commands/templates/list.ts
+++ src/commands/templates/list.ts
@@ -1,27 +1,10 @@
-import chalk = require('chalk');
 import { epilogue } from '../../helpers/util';
-import * as templateRepository from '../../repositories/templates';
+import * as templateService from '../../services/templates';
 
 export const command = 'list';
 export const desc = 'List all templates';
 export const builder = (yargs: any) => epilogue(yargs);
 
 export const handler = async function list({ isTTY }: { isTTY: boolean; }) {
-  const templates = await templateRepository.findAll();
-
-  if (templates.length < 1) {
-    console.log(chalk.red('No templates found'));
-    return;
-  }
-
-  if (isTTY) {
-    console.table(templates.map(template => ({
-      Id: template.id,
-      Name: template.name,
-      Description: template.description || '<none>',
-      'Last updated': template.updateTimestamp.toISOString(),
-    })));
-  } else {
-    templates.forEach(template => console.log(template));
-  }
+  await templateService.list(isTTY);
 };
--- src/commands/templates/sync.ts
+++ src/commands/templates/sync.ts
@@ -1,64 +1,37 @@
 import * as fs from 'fs';
 import * as ospath from 'path';
 import { epilogue } from '../../helpers/util';
-import { buildTemplates } from './util/buildTemplates';
-import { readTemplateFiles, readTemplateFolder, readTemplateJson } from './util/readTemplateFiles';
-import { uploadTemplate } from './util/uploadTemplate';
-import { removeFileNameExtension } from './util/utils';
+import * as templateService from '../../services/templates';
 
 export const command = 'sync';
 export const desc = 'Sync all templates in a directory with the ExH cloud';
-export const builder = (yargs: any) => epilogue(yargs).options({
-  path: {
-    demandOption: false,
-    describe: 'Directory containing the templates which need to be synced',
-    type: 'string',
-  },
-  template: {
-    demandOption: false,
-    describe: 'Template file to sync',
-    type: 'string',
-  },
-}).check(({ path, template }) => {
-  if (template && !fs.existsSync(ospath.join(process.cwd(), template))) {
-    throw new Error('please provide a valid file path for your template');
-  }
-  if (path && (!fs.existsSync(ospath.join(process.cwd(), path)) || fs.statSync(ospath.join(process.cwd(), path)).isFile())) {
-    throw new Error('please provide a valid directory path for your code');
-  }
+export const builder = (yargs: any) => epilogue(yargs)
+  .options({
+    path: {
+      demandOption: false,
+      describe: 'Directory containing the templates which need to be synced',
+      type: 'string',
+    },
+    template: {
+      demandOption: false,
+      describe: 'Template file to sync',
+      type: 'string',
+    },
+  })
+  .check(({ path, template }) => {
+    if (template && !fs.existsSync(ospath.join(process.cwd(), template))) {
+      throw new Error('please provide a valid file path for your template');
+    }
+    if (path && (!fs.existsSync(ospath.join(process.cwd(), path)) || fs.statSync(ospath.join(process.cwd(), path)).isFile())) {
+      throw new Error('please provide a valid directory path for your code');
+    }
 
-  if ((template && path) || (!template && !path)) {
-    throw new Error('Either path or template must be specified (but not both at the same time)');
-  }
-  return true;
-});
+    if ((template && path) || (!template && !path)) {
+      throw new Error('Either path or template must be specified (but not both at the same time)');
+    }
+    return true;
+  });
 
 export const handler = async ({ path, template }: { path?: string; template?: string; }) => {
-  if (path) {
-    await syncTargetDir(ospath.resolve(path));
-    return;
-  }
-
-  let templ = null;
-  if (fs.statSync(ospath.join(process.cwd(), template)).isFile()) {
-    /* In case a template file was provided */
-    templ = await readTemplateJson(template);
-  } else {
-    templ = await readTemplateFolder(template);
-  }
-
-  const templateName = removeFileNameExtension(ospath.basename(template));
-  await buildAndUploadTemplates({ [templateName]: templ });
+  await templateService.sync(path, template);
 };
-
-async function buildAndUploadTemplates(templateObject: any) {
-  const templates = await buildTemplates(templateObject);
-  for (const template of templates) {
-    await uploadTemplate(template);
-  }
-}
-
-export async function syncTargetDir(targetDir: string) {
-  const templateFilesByName = await readTemplateFiles(targetDir);
-  await buildAndUploadTemplates(templateFilesByName);
-}
